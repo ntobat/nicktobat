cb5540ed534d5f0f700b7b8137c4d35963565a9f
Nick <ntobat@gmail.com>
2016-04-07 17:51:01 +0930

    mobile portfolio

--- public/javascripts/scripty.js
+++ public/javascripts/scripty.js
@@ -1,7 +1,16 @@
 $(function(){
 
-	$('a.photos').on('click', function(){
-		
-	})
+	$('a.samplink').on('touchstart', function(e){
+		'use strict';
+		var link = $(this);
+		if (link.hasClass('hover')){
+			return true;
+		} else {
+			link.addClass('hover');
+			$('a.samplink').not(this).removeClass('hover');
+			e.preventDefault();
+			return false;
+		}
+	});
 
-})
+});
