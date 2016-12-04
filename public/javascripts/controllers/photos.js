0bf8748c3a112ee47cd09ce3e954c34257eda198
Nick <ntobat@gmail.com>
2016-12-04 10:08:32 -0500

    photo update 2

--- public/javascripts/controllers/photos.js
+++ public/javascripts/controllers/photos.js
@@ -2,32 +2,32 @@ angular.module('nickyT')
 .controller('PhotosCtrl', ['$scope', function PhotosCtrl($scope){
 	$scope.photos = [
 		{
-			src: '/images/toucan.jpg',
-			caption:
+			src: '/images/toucan.png',
+			caption: ' '
 		},
 		{
-			src: '/images/bear.jpg',
-			caption:
+			src: '/images/hoverbot.png',
+			caption: ' '
 		},
 		{
-			src: '/images/audrey.jpg',
-			caption:
+			src: '/images/bear.png',
+			caption: ' '
 		},
 		{
-			src: '/images/hoverbot.jpg',
-			caption:
+			src: '/images/audrey.png',
+			caption: ' '
 		},
 		{
-			src: '/images/outdoor.jpg',
-			caption:
+			src: '/images/keira.png',
+			caption: ' '
 		},
 		{
-			src: '/images/keira.jpg',
-			caption:
+			src: '/images/pinata.png',
+			caption: ' '
 		},
 		{
-			src: '/images/pinata.jpg',
-			caption:
+			src: '/images/outdoor.png',
+			caption: ' '
 		}
 	];
 }]);
