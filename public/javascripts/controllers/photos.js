3743344fffcd77e860a9bb917e54fcde299918c1
Nick <ntobat@gmail.com>
2016-12-04 09:52:30 -0500

    photos update

--- public/javascripts/controllers/photos.js
+++ public/javascripts/controllers/photos.js
@@ -2,48 +2,32 @@ angular.module('nickyT')
 .controller('PhotosCtrl', ['$scope', function PhotosCtrl($scope){
 	$scope.photos = [
 		{
-			src: '/images/photo1.jpg',
-			caption: 'Devil\'s Kitchen burgers in Hobart, Tasmania'
+			src: '/images/toucan.jpg',
+			caption:
 		},
 		{
-			src: '/images/photo2.jpg',
-			caption: 'Friendly Beach'
+			src: '/images/bear.jpg',
+			caption:
 		},
 		{
-			src: '/images/photo3.jpg',
-			caption: 'Sunrise at King\'s Beach I'
+			src: '/images/audrey.jpg',
+			caption:
 		},
 		{
-			src: '/images/photo4.jpg',
-			caption: 'Blueberry pancakes in Cataract Gorge'
+			src: '/images/hoverbot.jpg',
+			caption:
 		},
 		{
-			src: '/images/photo5.jpg',
-			caption: 'Ben Lomond'
+			src: '/images/outdoor.jpg',
+			caption:
 		},
 		{
-			src: '/images/photo6.jpg',
-			caption: 'Exultant atop Mt. Rufus'
+			src: '/images/keira.jpg',
+			caption:
 		},
 		{
-			src: '/images/photo7.jpg',
-			caption: 'Sheffield Caravan Park'
-		},
-		{
-			src: '/images/photo8.jpg',
-			caption: '*clink*'
-		},
-		{
-			src: '/images/photo9.jpg',
-			caption: 'Sunrise at King\'s Beach II'
-		},
-		{
-			src: '/images/photo10.jpg',
-			caption: 'Cornelian Bay'
-		},
-		{
-			src: '/images/photo11.jpg',
-			caption: 'Sunset behind a thunderhead'
-		},
+			src: '/images/pinata.jpg',
+			caption:
+		}
 	];
 }]);
